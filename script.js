e402b1551c753c2f09f9e1c41824939b0a8567d3
Nancy Blayney <nblayney@g.cofc.edu>
2016-03-21 17:02:17 -0400

    an underwater-scape

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+
+
+	var scrollheight = document.body.scrollHeight // height of entire document
+	var windowheight = window.innerHeight // height of browser window
+
+
+	function parallaxbubbles(){
+	  var scrolltop = $(window).scrollTop();
+	  var scrollamount = (scrolltop / (scrollheight-windowheight)) * 100;	
+	  $('.seahorse').css('top', -scrolltop * .3 + 'px');
+	  $('.turtle').css('top', -scrolltop * .6 + 'px');
+	 	$('.bubble').css('top', -scrolltop * .8 + 'px');
+	  $('.bubble2').css('top', -scrolltop * .9 + 'px');
+	  $('.shark').css('left', -100 + scrollamount + '%');
+	}
+	 
+	$(window).scroll(function(e){ 
+	  parallaxbubbles();
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
