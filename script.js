e56b8c1f40405416e30246a317af5ddd6a88229f
Nancy Blayney <nblayney@g.cofc.edu>
2016-10-17 15:09:35 -0400

    shark

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
 	function parallaxbubbles(){
 	  var scrolltop = $(window).scrollTop();
 	  var scrollamount = (scrolltop / (scrollheight-windowheight)) * 100;	
-	  $('.seahorse').css('top', -scrolltop * .3 + 'px');
-	  $('.turtle').css('top', -scrolltop * .6 + 'px');
+	  $('.seahorse').css('top', -scrolltop * .2 + 'px');
+	  $('.turtle').css('top', -scrolltop * .5 + 'px');
 	 	$('.bubble').css('top', -scrolltop * .8 + 'px');
 	  $('.bubble2').css('top', -scrolltop * .9 + 'px');
 	  $('.shark').css('left', -100 + scrollamount + '%');
